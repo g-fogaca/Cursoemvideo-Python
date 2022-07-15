e1b4182ad563972b0e58d2b6e5f9d9f3542b4de0
Gabriel Fogaça <107882728+g-fogaca@users.noreply.github.com>
2022-07-15 08:48:45 -0300

    ex

---
+++ ex007.py
@@ -0,0 +1,5 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+n1 = float(input('Nota na P1: '))
+n2 = float(input('Nota na P2: '))
+media = (n1 + n2) / 2
+print(f'A média entre {n1:.1f} e {n2:.1f} é igual a {media:.1f}')
