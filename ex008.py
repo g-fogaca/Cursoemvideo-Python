a82e0fceda5ad0978817e0da97171da63cc81227
Gabriel Fogaça <107882728+g-fogaca@users.noreply.github.com>
2022-07-16 10:25:48 -0300

    Create ex008.py

---
+++ ex008.py
@@ -0,0 +1,4 @@
+# Escreva um programa que leia uam valor em metros e o exiba convertido em centímetros e milimetros.
+d = float(input('Distância em metros: '))
+print(f'Em centimetros: {100*d:.2f}cm')
+print(f'Em milimetros: {1000*d:.2f}mm')
