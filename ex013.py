9a6e036e5b17cefee58c8d7e51c8282492e2edcb
Gabriel Fogaça <107882728+g-fogaca@users.noreply.github.com>
2022-07-21 13:02:28 -0300

    Create ex013.py

---
+++ ex013.py
@@ -0,0 +1,3 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+salario = float(input('Salário: R$'))
+print(f'O salário de R${salario:.2f} após um aumento de 15% passará a ser de R${salario*1.15:.2f}')
