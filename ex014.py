1429446ff65e1a55c6cc472adaca5403d12c0c1d
Gabriel Fogaça <107882728+g-fogaca@users.noreply.github.com>
2022-07-22 13:24:59 -0300

    Create ex014.py

---
+++ ex014.py
@@ -0,0 +1,4 @@
+# Escreva um programa que converta uma temperatura em ºC para ºF.
+c = float(input('Temperatura em ºC: '))
+f = 1.8*c + 32
+print(f'{c:.1f}ºC equivalem a uma temperatura de {f:.1f}ºF')
