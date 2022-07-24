14c66fda84c6b5436c5ef5c1b58dc0d4d3de95df
Gabriel Fogaça <107882728+g-fogaca@users.noreply.github.com>
2022-07-24 10:22:13 -0300

    Create ex016.py

---
+++ ex016.py
@@ -0,0 +1,3 @@
+# Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
+n = float(input('Digite um número qualquer: '))
+print(f'A porção inteira de {n} é {n//1:.0f}')
