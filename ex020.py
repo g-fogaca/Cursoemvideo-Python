c89d93190787b70d276f6a7f19e93cbd3d736db2
Gabriel Fogaça <107882728+g-fogaca@users.noreply.github.com>
2022-07-29 11:30:22 -0300

    Create ex020.py

---
+++ ex020.py
@@ -0,0 +1,10 @@
+# O mesmo professor do exercício anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa
+# que leia o nome de quatro alunos e mostre a ordem sorteada
+import random
+a1 = input('Primeiro aluno: ')
+a2 = input('Segundo aluno: ')
+a3 = input('Terceiro aluno: ')
+a4 = input('Quarto aluno: ')
+lista = [a1, a2, a3, a4]
+random.shuffle(lista)
+print(f'A ordem de apresentação será: {lista}')
